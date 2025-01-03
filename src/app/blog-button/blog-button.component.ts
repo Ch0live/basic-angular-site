0e4e6c85866006217eb1c5afd5fb17fc364343fa
Ch0live <dozeyis@gmail.com>
2025-01-03 16:46:21 +0000

    Make the button speak

--- src/app/blog-button/blog-button.component.ts
+++ src/app/blog-button/blog-button.component.ts
@@ -2,9 +2,12 @@ import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'blog-button',
-  templateUrl: './blog-button.component.html',
-  // standalone: true
+  templateUrl: './blog-button.component.html'
 })
 export class BlogButton {
   getNextParagraph = signal(false);
+  colourOfButton = "red"
+  greet() {
+    console.log('Hello there 👋');
+  }
 }
