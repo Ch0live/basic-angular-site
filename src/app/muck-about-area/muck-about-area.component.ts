67420106128bc9ca03a3b58cdcf5a8e329fb40ec
Ch0live <dozeyis@gmail.com>
2025-01-07 14:36:56 +0000

    Midway to a signal-logging effect

--- src/app/muck-about-area/muck-about-area.component.ts
+++ src/app/muck-about-area/muck-about-area.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Signal, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'blog-muck-about-area',
@@ -6,10 +6,18 @@ import { Component, computed, Signal, signal } from '@angular/core';
   template: `
     <div class="main">
       <h2>Signals</h2>
-      <button (click)="onClick()">Run</button>
+      <button (click)="onAlphaClick()">Alpha</button>
+      <button (click)="onBetaClick()">Beta</button>
       <p>
           {{ count() }}<br>{{ doubleCount() }}<br>{{ powerCount() }}
       </p>
+      <h3>
+        Log of events:
+      </h3>
+      <span>
+        {{ logOfEvents }}
+      </span>
+
     </div>
     `,
   styleUrl: './muck-about-area.component.css'
@@ -18,9 +26,21 @@ export class MuckAboutAreaComponent {
   readonly count = signal(0)
   readonly doubleCount = computed(() => this.count() * 2);
   readonly powerCount = computed(() => this.count() ** 2);
+  logOfEvents: string[] = [ this.computeCountLog() ];
 
-  onClick() {
-    // Set a signal
+  onAlphaClick() {
     this.count.update(() => this.count() + 1);
   }
+
+  onBetaClick() {
+    this.count.update(() => this.count() - 1);
+  }
+
+  computeCountLog(): string {
+    return `count: ${this.count} doubleCount: ${this.doubleCount} powerCount: ${this.powerCount}`;
+  }
 }
+
+effect(() => {
+  console.log(`The current count is: ${count()}`);
+});
