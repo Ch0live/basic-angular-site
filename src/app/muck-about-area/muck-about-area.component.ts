5e4c584e57f7c998abf3474a72ac956515d49f85
Ch0live <dozeyis@gmail.com>
2025-01-07 14:08:05 +0000

    Signal updates

--- src/app/muck-about-area/muck-about-area.component.ts
+++ src/app/muck-about-area/muck-about-area.component.ts
@@ -3,10 +3,28 @@ import { Component, signal } from '@angular/core';
 @Component({
   selector: 'blog-muck-about-area',
   imports: [],
-  templateUrl: './muck-about-area.component.html',
+  template: `
+    <div class="main">
+      <h2>Signals</h2>
+      <button (click)="onClick()">Run</button>
+      <p>
+          {{ label }}
+      </p>
+    </div>
+    `,
   styleUrl: './muck-about-area.component.css'
 })
 export class MuckAboutAreaComponent {
   readonly count = signal(0)
-  label = String(this.count);
+  label: string = "signal(0): " + String(this.count)
+
+  onClick() {
+    // Set a signal
+    this.count.set(3);
+    this.label = this.label + " count.set(3): " + String(this.count)
+
+    // Increment signal
+    this.count.update(value => value + 1)
+    this.label = this.label + " count.update(value => value + 1): " + String(this.count)
+  }
 }
---
+++ src/app/muck-about-area/signals/signals.service.spec.ts
@@ -0,0 +1,7 @@
+import { Signals } from './signals.service';
+
+describe('Signals', () => {
+  it('should create an instance', () => {
+    expect(new Signals()).toBeTruthy();
+  });
+});
