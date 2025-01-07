6b87efbf044c36dc3d4e4b2ac3c53c1006143c7e
Ch0live <dozeyis@gmail.com>
2025-01-07 15:49:04 +0000

    Making the class do the work

--- src/app/muck-about-area/muck-about-area.component.ts
+++ src/app/muck-about-area/muck-about-area.component.ts
@@ -14,9 +14,9 @@ import { Component, computed, effect, signal, WritableSignal } from '@angular/co
       <h3>
         Log of events:
       </h3>
-      <p>
+      <ul>
         {{ logOfEvents() }}
-      </p>
+      </ul>
     </div>
     `,
   styleUrl: './muck-about-area.component.css'
@@ -25,7 +25,7 @@ export class MuckAboutAreaComponent {
   readonly count = signal(0)
   readonly doubleCount = computed(() => this.count() * 2);
   readonly powerCount = computed(() => this.count() ** 2);
-  readonly logOfEvents: WritableSignal<string[]> = signal([]);
+  readonly logOfEvents: WritableSignal<HTMLUListElement> = signal([]);
 
   constructor() {
     effect(() => {
@@ -41,7 +41,9 @@ export class MuckAboutAreaComponent {
     this.count.update(() => this.count() - 1);
   }
 
-  computeCountLog(): string {
-    return `count: ${this.count} doubleCount: ${this.doubleCount} powerCount: ${this.powerCount}`;
+  computeCountLog(): HTMLUListElement {
+    const row = `count: ${this.count} doubleCount: ${this.doubleCount} powerCount: ${this.powerCount}`;
+    const li = document.createElement(`li`).textContent(row);
+    return li
   }
 }
