a5ed8e280d2d685e657499937e0bccee75ce7a34
Ch0live <dozeyis@gmail.com>
2025-01-09 08:34:38 +0000

    Add slider component

---
+++ src/app/blog-image/blog-slider/blog-slider.component.ts
@@ -0,0 +1,22 @@
+import {Component, computed, input} from '@angular/core';
+@Component({
+  selector: 'blog-slider',
+  imports: [],
+  template: `
+    <div>
+      <p>
+        {{ value }}
+      </p>
+    </div>
+  `,
+  styleUrl: 'blog-slider.component.css'
+})
+export class CustomSlider {
+
+  // Declare an input named 'value' with a default value of zero. 
+  value = input(0);
+
+  // Create a computed expression that reads the value input
+  label = computed(() => `The slider's value is ${this.value()}`); 
+  
+}
