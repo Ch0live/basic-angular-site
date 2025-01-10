01ba90b4cd7e674385ebf3e74e2ee7a8761d246d
Ch0live <dozeyis@gmail.com>
2025-01-10 14:39:42 +0000

    WIP trying to sneak in a coupl' of spies

--- src/app/article/article.service.spec.ts
+++ src/app/article/article.service.spec.ts
@@ -1,63 +1,23 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { ArticleService } from './article.service';
-import { staticPostList } from '../content/list-of-static-posts.json';
 import { HttpClient } from '@angular/common/http';
 
 describe('ArticleService', () => {
   let service: ArticleService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
+    const spy = jasmine.createSpyObj('HttpClient', ['get']);
     TestBed.configureTestingModule(
       {
-        providers: [HttpClient]
+        providers: [ArticleService, { provide: HttpClient, useValue: spy }]
       }
-    )
+    );
     service = TestBed.inject(ArticleService);
+    httpClientSpy = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-
-  // it('should have postsMetadata initialized', () => {
-  //   expect(service.postsMetadata).toEqual(staticPostList);
-  // });
-
-  // it('should return a post by path', () => {
-  //   const postPath = staticPostList[0].path;
-  //   const post = service.getByPath(postPath);
-  //   expect(post).toEqual(staticPostList[0]);
-  // });
-
-  // it('should throw an error if post is not found by path', () => {
-  //   expect(() => service.getByPath('non-existent-path')).toThrowError('Unable to find post in postsJson');
-  // });
-
-  // it('should fetch article content', () => {
-  //   const dummyArticle = '<h1>Test Article</h1>';
-  //   const path = 'test-article.html';
-
-  //   service.getArticle(path).subscribe(content => {
-  //     expect(content).toBe(dummyArticle);
-  //   });
-
-  //   const req = httpMock.expectOne(`/assets/articles/${path}`);
-  //   expect(req.request.method).toBe('GET');
-  //   req.flush(dummyArticle);
-  // });
-
-  // it('should handle error when fetching article content', () => {
-  //   const path = 'test-article.html';
-
-  //   service.getArticle(path).subscribe(
-  //     () => fail('should have failed with an error'),
-  //     (error) => {
-  //       expect(error.message).toContain('Error fetching html file for article:');
-  //     }
-  //   );
-
-  //   const req = httpMock.expectOne(`/assets/articles/${path}`);
-  //   req.flush('Error fetching html file for article', { status: 500, statusText: 'Server Error' });
-  // });
 });
