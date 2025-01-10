afd3c13cbda32e202d0ddf2608aacb5463975517
Ch0live <dozeyis@gmail.com>
2025-01-10 11:07:22 +0000

    Get traditional-blog tests running

--- src/app/traditional-blog/traditional-blog.component.spec.ts
+++ src/app/traditional-blog/traditional-blog.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TraditionalBlog } from './traditional-blog.component';
+import { ActivatedRoute } from '@angular/router';
 
 describe('TraditionalBlog', () => {
   let component: TraditionalBlog;
@@ -8,7 +9,13 @@ describe('TraditionalBlog', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TraditionalBlog]
+      imports: [TraditionalBlog], 
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { url: ['test-route'] } } // TODO: Understand why this provider fixes our ActivatedRoute dependency
+        }
+      ]
     })
     .compileComponents();
 
@@ -19,5 +26,18 @@ describe('TraditionalBlog', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load heading and welcome paragraph', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h')?.innerHTML).toContain('Welcome!');
+    expect(compiled.querySelector('p')?.innerHTML).toContain('I hope you find something you\'ll enjoy reading!');
+  });
+
+  it('should load a complete list of all blogs', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('ul')?.innerHTML).toContain('Cape May Beach Day');
+    expect(compiled.querySelector('ul')?.innerHTML).toContain('The Narrows in Zion');
+    expect(compiled.querySelector('ul')?.innerHTML).toContain('The Red Rock State');
   });
 });
