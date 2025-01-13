5983f38917f77dcf45c8a55ae05f3eb1c174f59d
Ch0live <dozeyis@gmail.com>
2025-01-13 09:41:18 +0000

    Split tests in two

--- src/app/image-grid/image-grid.component.spec.ts
+++ src/app/image-grid/image-grid.component.spec.ts
@@ -28,21 +28,38 @@ describe('ImageGrid', () => {
     const capeMayImage = images[0];
     expect(capeMayImage.tagName).toBe('IMG');
     expect(capeMayImage.src).toBe('http://localhost/assets/img/cape-may-beach-day/Image.png');
-    expect(capeMayImage.alt).toBe('Wildwood Crest Beach');
     
     const zionImage = images[1];
     expect(zionImage.tagName).toBe('IMG');
     expect(zionImage.src).toBe('http://localhost/assets/img/the-narrows-of-zion/IMG_3473.jpeg');
-    expect(zionImage.alt).toBe('Oderville Gulch in Zion National Park');
     
     const coloradoImage = images[2];
     expect(coloradoImage.tagName).toBe('IMG');
     expect(coloradoImage.src).toBe('http://localhost/assets/img/the-red-rock-state/IMG_2724.jpeg');
+  });
+
+  it('should have alt text for all images', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const images = compiled.querySelectorAll('img');
+    expect(images.length).toBe(3);
+    
+    const capeMayImage = images[0];
+    expect(capeMayImage.tagName).toBe('IMG');
+    expect(capeMayImage.alt).toBe('Wildwood Crest Beach');
+    
+    const zionImage = images[1];
+    expect(zionImage.tagName).toBe('IMG');
+    expect(zionImage.alt).toBe('Oderville Gulch in Zion National Park');
+    
+    const coloradoImage = images[2];
+    expect(coloradoImage.tagName).toBe('IMG');
     expect(coloradoImage.alt).toBe('An Elk in Rocky Mountain National Park');
   });
 
   // it should grow on mouseover
 
   // it should link straight to the blog article
+
+  // it should overlay the blog title
 
 });
