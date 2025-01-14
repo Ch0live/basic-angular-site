6099841d43930e778331665f998247eddb3bf077
Ch0live <dozeyis@gmail.com>
2025-01-14 15:12:15 +0000

    Add shuffling!

--- src/app/home/suggested-articles/suggested-articles.component.ts
+++ src/app/home/suggested-articles/suggested-articles.component.ts
@@ -1,17 +1,36 @@
 import { Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ShufflerComponent } from "./shuffler/shuffler.component";
+import { PostMetadata } from 'src/app/article/post.type';
 import { PostMetadataInputWrapper } from 'src/app/article/post.type';
 
 @Component({
   selector: 'blog-suggested-articles',
-  imports: [RouterLink, ShufflerComponent],
+  imports: [RouterLink],
   templateUrl: './suggested-articles.component.html',
   styleUrl: './suggested-articles.component.css'
 })
 export class SuggestedArticles {
-  shuffleArticles() {
-    console.log("Shuffling");
-  }
   postMetadata = input.required<PostMetadataInputWrapper>(); // TODO: Decide if this component is worth testing anymore
+  featuredPostMetadata: PostMetadata[] = [];
+
+  ngOnInit() {
+    this.shuffleArticles();
+  }
+  
+  shuffleArticles() {
+    const allArticles: PostMetadata[] = this.postMetadata().metadata;
+    this.featuredPostMetadata = this.pickRandomThree(allArticles);
+  }
+
+  pickRandomThree(arr: PostMetadata[]): PostMetadata[] {
+    if (arr.length > 3) {
+      return this.shuffleList(arr);
+    } else {
+      return this.shuffleList(arr).slice(0, 3);
+    }
+  };
+
+  shuffleList(arr: PostMetadata[]): PostMetadata[] {
+    return Array.from(arr).sort(() => 0.5 - Math.random());
+  }
 }
