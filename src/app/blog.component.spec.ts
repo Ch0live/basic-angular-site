7e8c1e730ccbc3dbd4bff7cc8436f19ce4295342
Ch0live <dozeyis@gmail.com>
2025-01-16 11:07:16 +0000

    Test default root

--- src/app/blog.component.spec.ts
+++ src/app/blog.component.spec.ts
@@ -1,18 +1,25 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Blog } from './blog.component';
-import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { routes } from './blog.routes';
 
-describe('AppComponent', () => {
+describe('Blog', () => {
+  let router: Router;
+  let fixture: ComponentFixture<Blog>;
+  let location: Location;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Blog],
-      providers: [
-        {
-          provide: ActivatedRoute, 
-          useValue: { snapshot: { url: ['test-route'] } }
-        }
-      ]
+      imports: [Blog, RouterTestingModule.withRoutes(routes)],
+      providers: []
     }).compileComponents();
+
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+    router.initialNavigation();
+    fixture = TestBed.createComponent(Blog);
   });
 
   it('should create the app', () => {
@@ -20,4 +27,12 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it('should navigate to /home by default', () => {
+    fixture.detectChanges()
+    fixture.whenStable().then(() => {
+      expect(location.path).toBe('/home');
+    });
+  });
+
 });
