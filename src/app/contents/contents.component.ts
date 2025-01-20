3a1478eed724ee0d85c119e0d5f56c510ea0e512
Ch0live <dozeyis@gmail.com>
2025-01-20 11:11:29 +0000

    Add SearchBar for filtering posts

--- src/app/contents/contents.component.ts
+++ src/app/contents/contents.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import postMetadata from '../../assets/static-post-metadata.json';
+import { SearchBarComponent } from "./search-bar/search-bar.component";
 
 @Component({
   selector: 'blog-contents',
-  imports: [RouterLink],
+  imports: [RouterLink, SearchBarComponent],
   templateUrl: 'contents.component.html',
   styleUrl: 'contents.component.css'
 })
